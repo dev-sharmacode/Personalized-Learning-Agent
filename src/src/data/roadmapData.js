fd7ed80e3f1059963b01a7b54d027e4a6b3982a0
dev-sharmacode <codersharma12@gmail.com>
2026-06-22 19:18:41 +0530

    Create roadmapData.js

---
+++ src/src/data/roadmapData.js
@@ -0,0 +1,24 @@
+const roadmap = [
+  {
+    id: 1,
+    title: "Introduction to AI",
+    completed: true,
+  },
+  {
+    id: 2,
+    title: "Machine Learning",
+    completed: true,
+  },
+  {
+    id: 3,
+    title: "Deep Learning",
+    completed: false,
+  },
+  {
+    id: 4,
+    title: "Generative AI",
+    completed: false,
+  },
+];
+
+export default roadmap;
