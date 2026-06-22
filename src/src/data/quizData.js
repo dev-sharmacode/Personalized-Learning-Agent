9c4205b23ec40713dd0817f1c96cf57c5a2e28b0
dev-sharmacode <codersharma12@gmail.com>
2026-06-22 19:19:15 +0530

    Create quizData.js

---
+++ src/src/data/quizData.js
@@ -0,0 +1,14 @@
+const quizData = [
+  {
+    question: "What does AI stand for?",
+    options: [
+      "Artificial Intelligence",
+      "Advanced Internet",
+      "Automated Interface",
+      "Artificial Integration",
+    ],
+    answer: "Artificial Intelligence",
+  },
+];
+
+export default quizData;
